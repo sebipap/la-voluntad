6f386739f60bffb65c627a2eca91bef011ea7dad
sebipapsu <sebipaps@gmail.com>
2022-10-10 15:52:55 -0300

    feat: add metadata to improve seo.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Head from "next/head";
 import Contact from "../components/Contact";
 import { Delivery } from "../components/Delivery/Delivery";
 import Footer from "../components/Footer";
@@ -10,6 +11,17 @@ import { Logo } from "../components/Logo/Logo";
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>La Voluntad</title>
+        <meta
+          name="description"
+          content="La Voluntad bakery bistro café San Isidro"
+        />
+        <meta
+          property="og:title"
+          content="La Voluntad bakery bistro café San Isidro"
+        />
+      </Head>
       <Logo />
       <Gallery />
       <Delivery />
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,8 +1,20 @@
 import { Heading } from "@chakra-ui/react";
+import Head from "next/head";
 
 const Menu = () => {
   return (
     <>
+      <Head>
+        <title>La Voluntad - menu</title>
+        <meta
+          name="description"
+          content="Encontrá todos los platos que tenemos para ofrecerte. La Voluntad bakery bistro café San Isidro"
+        />
+        <meta
+          property="og:title"
+          content="La Voluntad bakery bistro café San Isidro"
+        />
+      </Head>
       <Heading size={"lg"}>MENU</Heading>
       <iframe
         src="https://www.monline.com.ar/LaVoluntad"
